7b251a03a2cfef90b96eda70a72a01954532dbd1
chidera nwachukwu <chideranwachukwu@guest-wireless-10-106-135-163.umd.edu>
2019-09-24 14:13:17 -0400

    created game logic

--- memory_gamee/js/main.js
+++ memory_gamee/js/main.js
@@ -1,7 +1,7 @@
 console.log("Up and running!");
 
 const cards = ['queen', 'queen', 'king', 'king'];
-const cardsInPlay = [];
+const cardsInPlay = []
 const cardOne = cards[1];
 cardsInPlay.push('cardOne');
 console.log("User flipped queen")
@@ -9,10 +9,13 @@ const cardTwo = cards[3];
 cardsInPlay.push('cardTwo');
 console.log("User flipped king")
 
-if cardsInPlay[] === cardsInPlay {
-	alert ("You found a match.");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[3]) {
+		alert ("You found a match.");	
+	}
+	else {
+		alert("Sorry, try again.");
+	}
 }
-else {
-	alert("Sorry, try again.");
-}
+
 	
