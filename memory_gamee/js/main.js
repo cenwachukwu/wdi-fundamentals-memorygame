a0087fc390a8103195fab34a217e949477bf88d1
chidera nwachukwu <chideranwachukwu@guest-wireless-10-106-135-163.umd.edu>
2019-09-24 16:00:59 -0400

    created game logic

--- memory_gamee/js/main.js
+++ memory_gamee/js/main.js
@@ -1,23 +1,52 @@
-const cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = []
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[2]) {
-  console.log("You found a match!");
+  		alert ("You found a match!");
 	} 
 	else {
-  console.log("Sorry, try again.");
+  		alert ("Sorry, try again.");
 	}
 }
 
 function flipCard(cardId) {
 	if (cardsInPlay.length === 2) {
-		checkForMatch()
+		checkForMatch();
 	}
-	console.log ("User flipped" + cards[cardId])
-	cardsInPlay.push(cards[cardId])
+	console.log ("User flipped" + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log (cards[cardId].cardImage)
+	cardsInPlay.push(cards[cardId].cardImage);
+
+	console.log (cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].suit);
 }
 	
-flipCard(1)
-flipCard(2)
+flipCard(0);
+flipCard(2);
+
+
 
